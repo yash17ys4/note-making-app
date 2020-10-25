0afaf2cb856647f28723ae50a4911f72d1ac5336
yash17ys4 <yash17.ys4@gmail.com>
2020-10-25 22:20:49 +0530

    app.js created

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+//express app creation
+const app = express();
+
+//middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//defining PORT number
+var PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on ${PORT}`);
+});
